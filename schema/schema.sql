8df174023495f1f623c254b3aaadabbc7d86f9c7
dbc201 <denizbcakiroglu@gmail.com>
2021-11-21 23:40:03 -0500

    reformat schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,28 +1,28 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "accounts" (
-    "user_id"    INTEGER NOT NULL UNIQUE,
-    "user_email"    TEXT UNIQUE,
-    "username"    TEXT UNIQUE,
-    "user_password"    TEXT,
-    PRIMARY KEY("user_id" AUTOINCREMENT)
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"user_email"	TEXT UNIQUE,
+	"username"	TEXT UNIQUE,
+	"user_password"	TEXT,
+	PRIMARY KEY("user_id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "matches" (
-    "user_id"    INTEGER NOT NULL UNIQUE,
-    "user_connections"    BLOB,
-    PRIMARY KEY("user_id"),
-    FOREIGN KEY("user_id") REFERENCES "accounts"("user_id")
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"user_connections"	BLOB,
+	PRIMARY KEY("user_id"),
+	FOREIGN KEY("user_id") REFERENCES "accounts"("user_id")
 );
 CREATE TABLE IF NOT EXISTS "common_preferences" (
-    "preference_id"    INTEGER NOT NULL UNIQUE,
-    "preference_name"    TEXT UNIQUE,
-    "preference_type"    TEXT,
-    "user_weights"    BLOB,
-    PRIMARY KEY("preference_id" AUTOINCREMENT)
+	"preference_id"	INTEGER NOT NULL UNIQUE,
+	"preference_name"	TEXT UNIQUE,
+	"preference_type"	TEXT,
+	"user_weights"	BLOB,
+	PRIMARY KEY("preference_id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "user_preferences" (
-    "user_id"    INTEGER NOT NULL UNIQUE,
-    "user_preferences"    BLOB,
-    PRIMARY KEY("user_id"),
-    FOREIGN KEY("user_id") REFERENCES "accounts"("user_id")
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"user_preferences"	BLOB,
+	PRIMARY KEY("user_id"),
+	FOREIGN KEY("user_id") REFERENCES "accounts"("user_id")
 );
 COMMIT;
