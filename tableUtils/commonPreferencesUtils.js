bb594c846f5513fd4069bad64778f2cecda2b713
dbc201 <denizbcakiroglu@gmail.com>
2021-11-23 20:55:09 -0500

    CommonPreferencesUtils.hasPreference checks both name and type

--- tableUtils/commonPreferencesUtils.js
+++ tableUtils/commonPreferencesUtils.js
@@ -6,22 +6,22 @@ class CommonPreferencesUtils extends _Row{
         super("common_preferences", databaseWrapper, "preference_id");
     }
 
-    hasPreference(preference_name) {
-        let result = this.databaseWrapper.get(`SELECT * FROM ${this.table_name} WHERE preference_name = ?;`, [preference_name]);
+    hasPreference(preference_name, preference_type) {
+        let result = this.databaseWrapper.get(`SELECT * FROM ${this.table_name} WHERE preference_name = ? AND preference_type = ?;`, [preference_name, preference_type]);
         return !!result; // return false if undefined
     }
 
     setPreference(preference_name, preference_type, user_weights) {
-        if (this.hasPreference(preference_name)) {
-            this.databaseWrapper.run_query(`UPDATE ${this.table_name} SET user_weights=? WHERE preference_name=?`, [JSON.stringify(user_weights)]);
+        if (this.hasPreference(preference_name, preference_type)) {
+            this.databaseWrapper.run_query(`UPDATE ${this.table_name} SET user_weights=? WHERE preference_name=? AND preference_type = ?`, [JSON.stringify(user_weights), preference_name, preference_type]);
         }
         else {
             this.databaseWrapper.run_query(`INSERT INTO ${this.table_name}(preference_name, preference_type, user_weights) VALUES(?,?,?)`, [preference_name, preference_type, JSON.stringify(user_weights)]);
         }
     }
 
-    getPreferenceData(preference_name) {
-        return this.databaseWrapper.run_query(`SELECT * FROM ${this.table_name} WHERE preference_name=?`, [preference_name]);
+    getPreferenceData(preference_name, preference_type) {
+        return this.databaseWrapper.run_query(`SELECT * FROM ${this.table_name} WHERE preference_name=? AND preference_type=?`, [preference_name, preference_type]);
     }
 }
 
