372a45a436b1df9f403b64b9c511260d4e6335c6
dbc201 <denizbcakiroglu@gmail.com>
2022-01-09 22:47:48 -0500

    add preference_type_weight and preference_identifier_weight

--- schema/schema.sql
+++ schema/schema.sql
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS "user_friends"
 (
     "user_id" INTEGER NOT NULL UNIQUE,
     "friends" BLOB,
-    PRIMARY KEY ("user_id"),
-    FOREIGN KEY ("user_id") REFERENCES "accounts" ("user_id")
+    FOREIGN KEY ("user_id") REFERENCES "accounts" ("user_id"),
+    PRIMARY KEY ("user_id")
 );
 CREATE TABLE IF NOT EXISTS "user_languages"
 (
@@ -24,16 +24,18 @@ CREATE TABLE IF NOT EXISTS "user_languages"
 );
 CREATE TABLE IF NOT EXISTS "user_preferences"
 (
-    "user_id"               INTEGER NOT NULL,
-    "preference_type"       TEXT,
-    "preference_identifier" TEXT,
+    "user_id"                      INTEGER NOT NULL,
+    "preference_type"              TEXT,
+    "preference_identifier"        TEXT,
+    "preference_type_weight"       INTEGER DEFAULT 0,
+    "preference_identifier_weight" INTEGER DEFAULT 0,
     FOREIGN KEY ("user_id") REFERENCES "accounts" ("user_id")
 );
 CREATE TABLE IF NOT EXISTS "user_connections"
 (
     "user_id1" INTEGER NOT NULL,
     "user_id2" INTEGER NOT NULL,
-    "weight"   INTEGER NOT NULL,
+    "weight"   INTEGER DEFAULT 0,
     "matched"  INTEGER DEFAULT 0,
     FOREIGN KEY ("user_id1") REFERENCES "accounts" ("user_id"),
     FOREIGN KEY ("user_id2") REFERENCES "accounts" ("user_id")
