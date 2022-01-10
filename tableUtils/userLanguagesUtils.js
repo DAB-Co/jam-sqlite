7c0c8f63f441480e51571fb855a5919914e3da26
dbc201 <denizbcakiroglu@gmail.com>
2022-01-09 22:34:36 -0500

    adding and removing languages is done in arrays

--- tableUtils/userLanguagesUtils.js
+++ tableUtils/userLanguagesUtils.js
@@ -17,19 +17,52 @@ class UserLanguagesUtils extends _Row {
      */
     addUser(user_id, languages) {
         for (let i=0; i<languages.length; i++) {
-            this.databaseWrapper.run_query(`INSERT INTO ${this.table_name} (user_id, language) VALUES (?,?)`, [user_id, languages[i]]);
+            this.databaseWrapper.run_query(`INSERT INTO ${this.table_name} (user_id, language) VALUES (?,?)`, [user_id, languages[i].toUpperCase()]);
         }
     }
 
     /**
      *
      * @param user_id
-     * @returns {string[] | undefined} languages spoken by a user's id
+     * @param {string[]} languages
+     */
+    addLanguages(user_id, languages) {
+        for (let i=0; i<languages.length; i++) {
+            this.databaseWrapper.run_query(`INSERT INTO ${this.table_name} (user_id, language) VALUES (?,?)`, [user_id, languages[i].toUpperCase()]);
+        }
+    }
+
+    /**
+     * remove array of languages from user
+     *
+     * if the language doesn't exist, nothing wil change
+     *
+     * @param user_id
+     * @param {string[]} languages
+     */
+    removeLanguages(user_id, languages) {
+        for (let i=0; i<languages.length; i++) {
+            languages[i] = languages[i].toUpperCase();
+        }
+        let query_condition = "";
+        for (let i=0; i<languages.length; i++) {
+            query_condition += " language=?";
+            if (i !== languages.length-1) {
+                query_condition += " OR "
+            }
+        }
+        this.databaseWrapper.run_query(`DELETE FROM ${this.table_name} WHERE user_id=? AND ${query_condition}`, [user_id].concat(languages));
+    }
+
+    /**
+     *
+     * @param user_id
+     * @returns {string[]} languages spoken by a user's id
      */
     getUserLanguages(user_id) {
         let rows = this.databaseWrapper.get_all(`SELECT language FROM ${this.table_name} WHERE user_id = ?`, [user_id]);
         if (rows === undefined) {
-            return undefined;
+            return [];
         }
         else {
             let languages = [];
@@ -43,9 +76,12 @@ class UserLanguagesUtils extends _Row {
     /**
      *
      * @param {string[]} languages
-     * @returns {number[] | undefined} user_ids
+     * @returns {number[]} user_ids
      */
     getUsersWithTheSameLanguages(languages) {
+        for (let i=0; i<languages.length; i++) {
+            languages[i] = languages[i].toUpperCase();
+        }
         let query_condition = "";
         for (let i=0; i<languages.length; i++) {
             query_condition += " language=?";
@@ -55,7 +91,7 @@ class UserLanguagesUtils extends _Row {
         }
         let rows = this.databaseWrapper.get_all(`SELECT user_id FROM ${this.table_name} WHERE ${query_condition}`, languages);
         if (rows === undefined) {
-            return undefined;
+            return [];
         }
         else {
             let speakers = [];
--- test/userLanguageUtilsTest.js
+++ test/userLanguageUtilsTest.js
@@ -12,18 +12,33 @@ async function main() {
 
     userLanguagesUtils.addUser(1, ["English", "Turkish"]);
     let user1_languages = userLanguagesUtils.getUserLanguages(1);
-    console.assert(user1_languages[0] === "English" && user1_languages[1] === "Turkish", "getting user 1 languages failed");
+    console.assert(user1_languages[0] === "ENGLISH" && user1_languages[1] === "TURKISH", "getting user 1 languages failed");
 
     userLanguagesUtils.addUser(2, ["English", "Hindu"]);
     let user2_languages = userLanguagesUtils.getUserLanguages(2);
-    console.assert(user2_languages[0] === "English" && user2_languages[1] === "Hindu", "getting user 2 languages failed");
+    console.assert(user2_languages[0] === "ENGLISH" && user2_languages[1] === "HINDU", "getting user 2 languages failed");
 
     userLanguagesUtils.addUser(3, ["Hindu"]);
     let user3_languages = userLanguagesUtils.getUserLanguages(3);
-    console.assert(user3_languages[0] === "Hindu", "getting user 3 languages failed");
+    console.assert(user3_languages[0] === "HINDU", "getting user 3 languages failed");
 
     let hindu_speakers = userLanguagesUtils.getUsersWithTheSameLanguages(["Hindu"]);
     console.assert(hindu_speakers.indexOf(1) === -1 && hindu_speakers.indexOf(2) !== -1 && hindu_speakers.indexOf(3) !== -1, "getting hindu speakers failed");
+
+    userLanguagesUtils.removeLanguages(2, ["hindu"]);
+    user2_languages = userLanguagesUtils.getUserLanguages(2);
+    console.assert(user2_languages.indexOf("HINDU") === -1 && user2_languages.indexOf("ENGLISH") !== -1, "deleting hindu from user 2 failed");
+
+    userLanguagesUtils.addLanguages(3, ["TuRkiSH"]);
+    user3_languages = userLanguagesUtils.getUserLanguages(3);
+    console.assert(user3_languages.indexOf("HINDU") !== -1 && user3_languages.indexOf("TURKISH") !== -1, "adding turkish to user 3 failed");
+
+    let turkish_speakers = userLanguagesUtils.getUsersWithTheSameLanguages(["tUrKISH"]);
+    console.assert(turkish_speakers.indexOf(1) !== -1 && turkish_speakers.indexOf(2) === -1 && turkish_speakers.indexOf(3) !== -1, "getting turkish speakers failed");
+
+    let english_and_hindu_speakers = userLanguagesUtils.getUsersWithTheSameLanguages(["english", "hindu"]);
+    console.assert(english_and_hindu_speakers.indexOf(1) !== -1 && english_and_hindu_speakers.indexOf(2) !== -1 && english_and_hindu_speakers.indexOf(3) !== -1,
+        "failed getting english and hindu speakers")
 }
 
 main().then();
