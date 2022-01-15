09e2cebc40d6bf422cd6d951c4a8402985054e17
dbc201 <denizbcakiroglu@gmail.com>
2022-01-14 21:52:21 -0500

    add functions

--- tableUtils/userPreferencesUtils.js
+++ tableUtils/userPreferencesUtils.js
@@ -10,15 +10,81 @@ class UserPreferencesUtils extends _Row {
         super("user_preferences", database);
     }
 
-    addPreference(user_id, preference_type, preference_name, preference_type_weight, preference_name_weight) {
-
+    /**
+     * add new preference to the database (be careful, this doesn't check for duplicates)
+     *
+     * @param user_id
+     * @param preference_type
+     * @param preference_identifier
+     * @param preference_type_weight
+     * @param preference_identifier_weight
+     */
+    addPreference(user_id, preference_type, preference_identifier, preference_type_weight, preference_identifier_weight) {
+        this.databaseWrapper.run_query(`INSERT INTO ${this.table_name} 
+    (user_id, preference_type, preference_identifier, preference_type_weight, preference_name_weight) VALUES(?,?,?,?,?,)`,
+            [user_id, preference_type, preference_identifier, preference_type_weight, preference_identifier_weight]);
     }
 
-    updatePreference(user_id, preference_type, preference_name, preference_type_weight, preference_name_weight) {
-
+    /**
+     * get specific preference data for a user
+     *
+     * @param user_id
+     * @param preference_type
+     * @param preference_identifier
+     * @returns {json} {
+        "user_id",
+        "preference_type",
+        "preference_identifier",
+        "preference_type_weight,
+        "preference_identifier_weight",}
+     */
+    getUserPreference(user_id, preference_type, preference_identifier) {
+        return this.databaseWrapper.get(`SELECT * FROM ${this.table_name} WHERE user_id=? AND preference_type=? AND preference_identifier=?`,
+            [user_id, preference_type, preference_identifier]);
     }
 
-    getPreferences(user_ids) {
+    /**
+     * update preference type weight and preference identifier weight
+     *
+     * @param user_id
+     * @param preference_type
+     * @param preference_identifier
+     * @param preference_type_weight
+     * @param preference_identifier_weight
+     */
+    updateUserPreferenceWeights(user_id, preference_type, preference_identifier, preference_type_weight, preference_identifier_weight) {
+        this.databaseWrapper.run_query(`UPDATE ${this.table_name} SET 
+                 preference_type_weight=?, preference_identifier_weight=? WHERE user_id=? AND preference_type=? AND preference_identifier=?`,
+            [preference_type_weight, preference_identifier_weight, user_id, preference_type, preference_identifier]);
+    }
+
+    /**
+     * update preference identifier weight of a given user
+     *
+     * @param user_id
+     * @param preference_type
+     * @param preference_identifier
+     * @param preference_identifier_weight
+     */
+    updatePreferenceIdentifierWeight(user_id, preference_type, preference_identifier, preference_identifier_weight) {
+        this.databaseWrapper.run_query(`UPDATE ${this.table_name} SET preference_identifier_weight=? WHERE user_id=? AND preference_type=? AND preference_identifier=?`,
+            [preference_identifier_weight, user_id, preference_type, preference_identifier]);
+    }
+
+    /**
+     * update preference type weight of a given user
+     *
+     * @param user_id
+     * @param preference_type
+     * @param preference_identifier
+     * @param preference_type_weight
+     */
+    updatePreferenceTypeWeight(user_id, preference_type, preference_identifier, preference_type_weight) {
+        this.databaseWrapper.run_query(`UPDATE ${this.table_name} SET preference_type_weight=? WHERE user_id=? AND preference_type=? AND preference_identifier=?`,
+            [preference_type_weight, user_id, preference_type, preference_identifier]);
+    }
+
+    getCommonPreferences(user_ids) {
 
     }
 }
