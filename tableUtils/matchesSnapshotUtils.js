edcc8c64cd31f0484829f6b789a9fa12ee6350b4
dbc201 <denizbcakiroglu@gmail.com>
2022-04-17 07:55:51 -0400

    fill functions

--- tableUtils/matchesSnapshotUtils.js
+++ tableUtils/matchesSnapshotUtils.js
@@ -7,15 +7,46 @@ function isObject(objValue) {
 }
 
 class MatchesSnapshotUtils extends _Row {
+    constructor(database) {
+        super("matches_snapshot", database, "snapshot_id");
+    }
+
     insertSnapshot(snapshot) {
-
+        if (!isObject(snapshot)) {
+            throw new Error("data is not an object");
+        }
+        this.databaseWrapper.run_query(`INSERT INTO ${this.table_name} (snapshot) VALUES(?)`, [JSON.stringify(snapshot)]);
     }
 
+    /**
+     *
+     *
+     * @param snapshot_id
+     * @returns {JSON|undefined}
+     */
     getSnapshot(snapshot_id) {
+        let res = this.getColumnByPrimaryKey(snapshot_id, "snapshot");
 
+        if (res === undefined) {
+            return undefined;
+        }
+        else {
+            return JSON.parse(res);
+        }
     }
 
+    /**
+     *
+     * @returns {JSON|undefined}
+     */
     getLastSnapshot() {
+        let res = this.databaseWrapper.get(`SELECT snapshot FROM ${this.table_name} ORDER BY ${this.primary_key} DESC LIMIT 1`);
 
+        if (res === undefined) {
+            return undefined;
+        }
+        else {
+            return JSON.parse(res);
+        }
     }
 }
