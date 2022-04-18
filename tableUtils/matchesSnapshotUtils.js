aa785d8b1ff2035f079f8dcfebe69be021c0c5fb
dbc201 <denizbcakiroglu@gmail.com>
2022-04-17 20:29:56 -0400

    rawSnapshot

--- tableUtils/matchesSnapshotUtils.js
+++ tableUtils/matchesSnapshotUtils.js
@@ -1,54 +1,35 @@
 const path = require("path");
 const _Row = require(path.join(__dirname, "_row.js"));
 
-//https://www.w3docs.com/snippets/javascript/how-to-check-if-a-value-is-an-object-in-javascript.html
-function isObject(objValue) {
-    return objValue && typeof objValue === 'object' && objValue.constructor === Object;
-}
-
 class MatchesSnapshotUtils extends _Row {
     constructor(database) {
         super("matches_snapshot", database, "snapshot_id");
     }
 
-    insertSnapshot(snapshot) {
-        if (!isObject(snapshot)) {
-            throw new Error("data is not an object");
-        }
-        let s = JSON.stringify(snapshot, (key, value) => (value instanceof Map || value instanceof Set ? [...value] : value));
-        this.databaseWrapper.run_query(`INSERT INTO ${this.table_name} (snapshot) VALUES(?)`, [JSON.stringify(s)]);
+    /**
+     *
+     * @param {string} snapshot
+     */
+    insertRawSnapshot(snapshot) {
+        this.databaseWrapper.run_query(`INSERT INTO ${this.table_name} (snapshot) VALUES(?)`, [snapshot]);
     }
 
     /**
      *
      *
      * @param snapshot_id
-     * @returns {JSON|undefined}
+     * @returns {string | undefined}
      */
-    getSnapshot(snapshot_id) {
-        let res = this.getColumnByPrimaryKey(snapshot_id, "snapshot");
-
-        if (res === undefined) {
-            return undefined;
-        }
-        else {
-            return JSON.parse(res, (key, value) => (value instanceof Map || value instanceof Set ? [...value] : value));
-        }
+    getRawSnapshot(snapshot_id) {
+        return this.getColumnByPrimaryKey(snapshot_id, "snapshot");
     }
 
     /**
      *
-     * @returns {JSON|undefined}
+     * @returns {string | undefined}
      */
     getLastSnapshot() {
-        let res = this.databaseWrapper.get(`SELECT snapshot FROM ${this.table_name} ORDER BY ${this.primary_key} DESC LIMIT 1`);
-
-        if (res === undefined) {
-            return undefined;
-        }
-        else {
-            return JSON.parse(res);
-        }
+        return this.databaseWrapper.get(`SELECT snapshot FROM ${this.table_name} ORDER BY ${this.primary_key} DESC LIMIT 1`);
     }
 }
 
---
+++ test/matchesSnapshotUtilsTest.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const setup = require(path.join(__dirname, "setup.js"));
+const assert = require("assert");
+
+const jam_sqlite = setup.jam_sqlite;
+
+
+describe(__filename, function() {
+
+});
