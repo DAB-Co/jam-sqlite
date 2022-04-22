8b49348fc5d19311b4b8f667b4ae7912a2dc5aab
atillaturkmen <turkmenatilla522@gmail.com>
2022-04-22 23:06:26 +0300

    avatar utils finished

--- tableUtils/userAvatarsUtils.js
+++ tableUtils/userAvatarsUtils.js
@@ -7,23 +7,25 @@ class UserAvatarsUtils extends _Row {
     }
 
     updateProfilePic(user_id, original, small) {
-        this.databaseWrapper.run_query(`UPDATE ${this.table_name}
+        return this.databaseWrapper.run_query(`UPDATE ${this.table_name}
         SET (big_avatar, small_avatar)=(?, ?)
         WHERE user_id=?;`, [original, small, user_id]);
     }
 
     removeProfilePic(user_id) {
-        this.databaseWrapper.run_query(`UPDATE ${this.table_name}
+        return this.databaseWrapper.run_query(`UPDATE ${this.table_name}
         SET (big_avatar, small_avatar)=(NULL, NULL)
         WHERE user_id=?;`, [user_id]);
     }
 
     getSmallProfilePic(user_id) {
-        this.databaseWrapper.run_query(`SELECT (small_avatar) FROM ${this.table_name} WHERE user_id=?;`, [user_id]);
+        let arr = this.databaseWrapper.get_all(`SELECT (small_avatar) FROM ${this.table_name} WHERE user_id=?;`, [user_id]);
+        return arr[0]["small_avatar"];
     }
 
     getOriginalProfilePic(user_id) {
-        this.databaseWrapper.run_query(`SELECT (big_avatar) FROM ${this.table_name} WHERE user_id=?;`, [user_id]);
+        let arr = this.databaseWrapper.get_all(`SELECT (big_avatar) FROM ${this.table_name} WHERE user_id=?;`, [user_id]);
+        return arr[0]["big_avatar"];
     }
 }
 
